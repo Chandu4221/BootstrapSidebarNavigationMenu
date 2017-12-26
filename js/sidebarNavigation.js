dcfb4793fab8856dafceac77ed028b096df6da26
Chandu4221 <chndraja@gmail.com>
2017-12-27 00:06:48 +0530

    first commit

---
+++ js/sidebarNavigation.js
@@ -0,0 +1,44 @@
+window.onload = function()
+{
+  if (window.jQuery)
+  {
+$(document).ready(function(){
+$(".sidebarNavigation").ready(function(){
+  var newId = $(".sidebarNavigation .navbar-collapse").attr("id");
+  var bgColor = $(".sidebarNavigation").css("backgroundColor");
+  $(".sidebarNavigation .navbar-collapse")
+    .hide()
+    .clone()
+    .appendTo("body")
+    .attr("id",newId)
+    .removeAttr("class")
+    .addClass("sideMenu").show();
+    $("body").append("<div class='overlay'></div>");
+});
+$(".navbar-toggle").on("click",function(){
+  var classes = $(".sidebarNavigation").attr("class");
+  $(".sideMenu").addClass(classes).removeClass("navbar sidebarNavigation");
+  $(".sideMenu, .overlay").toggleClass("open");
+  $(".overlay").on("click",function(){
+    $(this).removeClass("open");
+    $(".sideMenu").removeClass("open");
+  });
+});
+ 
+$(window).resize(function(){
+  if($(".navbar-toggle").is(":hidden")){
+  $(".sideMenu, .overlay").hide();
+  }
+else{
+  $(".sideMenu, .overlay").show();
+}
+});
+});
+
+}
+else
+{
+    console.log("sidebarNavigation Requires jQuery");
+}
+}
+
